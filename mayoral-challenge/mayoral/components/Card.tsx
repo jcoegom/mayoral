c874c0bdfd52d2eb8d765ffb05f8c2761b6117b7
jcoegom@gmail.com <jcoegom@gmail.com>
2023-01-02 18:20:43 +0100

    wip-card image button

--- mayoral-challenge/mayoral/components/Card.tsx
+++ mayoral-challenge/mayoral/components/Card.tsx
@@ -1,17 +1,30 @@
 import Image from "next/image";
+import styles from "../styles/Card.module.css";
 
 type CardProps = {
   srcImg: string;
   altImg?: string;
+  description: string;
   Content: React.ReactNode;
 };
 
-const Card = ({ srcImg, altImg, Content }: CardProps) => {
+const DESCRIPTION_MAX_SIZE = 20;
+
+const Card = ({ srcImg, altImg, description, Content }: CardProps) => {
+  let descriptionToShow = description ? description : "No hay descripción";
+  descriptionToShow =
+    descriptionToShow.length > DESCRIPTION_MAX_SIZE
+      ? descriptionToShow.substring(0, DESCRIPTION_MAX_SIZE - 1) + "..."
+      : descriptionToShow;
+
   return (
-    <div>
-      <Image src={""} width={128} height={77} alt={""} />
+    <div className={styles.cardContainer}>
+      <span className={styles.cardImg}>
+        <Image src={srcImg} width={180} height={200} alt={altImg || ""} />
+      </span>
+      <div className={styles.cardDescription}>{descriptionToShow}</div>
       {Content}
-      <button>AÑADIR</button>
+      <button className={styles.cardButton}>AÑADIR</button>
     </div>
   );
 };
--- mayoral-challenge/mayoral/pages/index.tsx
+++ mayoral-challenge/mayoral/pages/index.tsx
@@ -2,13 +2,19 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "../styles/Home.module.css";
+import Card from "../components/Card";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-      <div>Hi</div>
+      <Card
+        srcImg="/imgs/polo-manga-larga-combinado-para-nino_id_12-04182-075-M-4.jpg"
+        altImg=""
+        description="Esta es la descripcion"
+        Content={<h1>Content</h1>}
+      />
     </>
   );
 }
