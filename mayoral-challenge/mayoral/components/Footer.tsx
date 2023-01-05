0be9da091bd1dba5a9125041d55f5a169475c8ee
jcoegom@gmail.com <jcoegom@gmail.com>
2023-01-05 09:29:42 +0100

    fixed reponsiviness

--- mayoral-challenge/mayoral/components/Footer.tsx
+++ mayoral-challenge/mayoral/components/Footer.tsx
@@ -1,5 +1,16 @@
+import Gap from "./Gap";
+
 const Footer = () => {
-  return <footer className="footer">Mayoral: All rights reserved</footer>;
+  return (
+    <footer className="footer">
+      <div>
+        © 2023 MAYORAL Todos los derechos reservados | aviso legal | Política de
+        privacidad
+      </div>
+      <Gap size={"10px"} />
+      <div>Teléfono de contacto: 988 88 88 88</div>
+    </footer>
+  );
 };
 
 export default Footer;
